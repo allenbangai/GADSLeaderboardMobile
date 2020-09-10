761ecd1a141e22fcc1e5dc98b8264b4f3309b1fd
=allenbangai <=allenkamadje@gmail.com>
2020-09-10 18:34:44 +0100

    code for submission activity xml part 4 final

--- app/src/main/java/com/example/gadsleaderboardmobile/SubmissionActivity.java
+++ app/src/main/java/com/example/gadsleaderboardmobile/SubmissionActivity.java
@@ -3,12 +3,29 @@ package com.example.gadsleaderboardmobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SubmissionActivity extends AppCompatActivity {
 
+    private EditText firstName, lastName, emailAddress, githubLink;
+    private TextView submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submission);
+
+        submit = findViewById(R.id.submit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                submitProject();
+            }
+        });
+    }
+
+    private void submitProject() {
+
     }
 }
