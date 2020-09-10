4c98bee01045c6bd71dfd77694124abd03f75c1c
=allenbangai <=allenkamadje@gmail.com>
2020-09-10 21:04:57 +0100

    code to submit project part1: declaring variables for use in submission functio

--- app/src/main/java/com/example/gadsleaderboardmobile/SubmissionActivity.java
+++ app/src/main/java/com/example/gadsleaderboardmobile/SubmissionActivity.java
@@ -3,9 +3,12 @@ package com.example.gadsleaderboardmobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.regex.Pattern;
 
 public class SubmissionActivity extends AppCompatActivity {
 
@@ -15,6 +18,11 @@ public class SubmissionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submission);
+
+        firstName = findViewById(R.id.first_name);
+        lastName = findViewById(R.id.last_name);
+        emailAddress = findViewById(R.id.email_address);
+        githubLink = findViewById(R.id.github_project_link);
 
         submit = findViewById(R.id.submit);
         submit.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +34,26 @@ public class SubmissionActivity extends AppCompatActivity {
     }
 
     private void submitProject() {
+        String firstNameStr, lastNameStr, emailAddressStr, githubLinkStr;
+        firstNameStr = firstName.getText().toString();
+        lastNameStr = lastName.getText().toString();
+        emailAddressStr = emailAddress.getText().toString();
+        githubLinkStr = githubLink.getText().toString();
 
+        if(firstNameStr.isEmpty()){
+
+        }else if(lastNameStr.trim().isEmpty()){
+
+        }else if(emailAddressStr.trim().isEmpty()){
+
+        }else if(!Patterns.EMAIL_ADDRESS.matcher(emailAddressStr).matches()){
+
+        }else if(!Patterns.WEB_URL.matcher(githubLinkStr).matches()){
+
+        }else if(githubLinkStr.trim().isEmpty()){
+
+        }else{
+
+        }
     }
 }
