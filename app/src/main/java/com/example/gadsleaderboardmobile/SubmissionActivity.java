3cc72a43fd1455b83a9b94c5f6b5a766a4f5e8b6
=allenbangai <=allenkamadje@gmail.com>
2020-09-12 12:07:09 +0100

    code for confirm submisssion xml complete

--- app/src/main/java/com/example/gadsleaderboardmobile/SubmissionActivity.java
+++ app/src/main/java/com/example/gadsleaderboardmobile/SubmissionActivity.java
@@ -8,16 +8,20 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.gadsleaderboardmobile.Util.Helper;
+
 import java.util.regex.Pattern;
 
 public class SubmissionActivity extends AppCompatActivity {
 
     private EditText firstName, lastName, emailAddress, githubLink;
     private TextView submit;
+    private Helper helper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submission);
+        helper = new Helper(this);
 
         firstName = findViewById(R.id.first_name);
         lastName = findViewById(R.id.last_name);
@@ -40,18 +44,24 @@ public class SubmissionActivity extends AppCompatActivity {
         emailAddressStr = emailAddress.getText().toString();
         githubLinkStr = githubLink.getText().toString();
 
-        if(firstNameStr.isEmpty()){
-
+        if(firstNameStr.trim().isEmpty()){
+            helper.toastMessage("first name field is empty");
+            firstName.requestFocus();
         }else if(lastNameStr.trim().isEmpty()){
-
+            helper.toastMessage("last name field is empty");
+            lastName.requestFocus();
         }else if(emailAddressStr.trim().isEmpty()){
-
+            helper.toastMessage("email address field is empty");
+            emailAddress.requestFocus();
         }else if(!Patterns.EMAIL_ADDRESS.matcher(emailAddressStr).matches()){
-
+            helper.toastMessage("email address is not correct");
+            emailAddress.requestFocus();
         }else if(!Patterns.WEB_URL.matcher(githubLinkStr).matches()){
-
+            helper.toastMessage("github web link is not correct");
+            githubLink.requestFocus();
         }else if(githubLinkStr.trim().isEmpty()){
-
+            helper.toastMessage("web link field is empty");
+            githubLink.requestFocus();
         }else{
 
         }
