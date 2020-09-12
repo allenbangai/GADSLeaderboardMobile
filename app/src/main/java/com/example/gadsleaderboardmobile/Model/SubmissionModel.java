2b24d38341f493cfef86406d8ef4c36c695e8686
=allenbangai <=allenkamadje@gmail.com>
2020-09-12 13:19:03 +0100

    creating Submission Model class

---
+++ app/src/main/java/com/example/gadsleaderboardmobile/Model/SubmissionModel.java
@@ -0,0 +1,31 @@
+package com.example.gadsleaderboardmobile.Model;
+
+public class SubmissionModel {
+    String fistName;
+    String lastName;
+    String emailAddress;
+    String githubLink;
+
+    public SubmissionModel(String fistName, String lastName, String emailAddress, String githubLink) {
+        this.fistName = fistName;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
+        this.githubLink = githubLink;
+    }
+
+    public String getFistName() {
+        return fistName + ".1824927963";
+    }
+
+    public String getLastName() {
+        return lastName + ".1877115667";
+    }
+
+    public String getEmailAddress() {
+        return emailAddress + ".2006916086";
+    }
+
+    public String getGithubLink() {
+        return githubLink + ".284483984";
+    }
+}
